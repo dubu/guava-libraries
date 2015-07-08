7bc1474d6f168d4a0b08f82dcd732f6ec5dba5cd
kozazz <kozazz@hanmail.net>
2015-07-08 18:48:19 +0900

    base test add

--- guava-tests/test/com/google/common/base/DubuBaseTest.java
+++ guava-tests/test/com/google/common/base/DubuBaseTest.java
@@ -1,9 +1,12 @@
 package com.google.common.base;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -60,7 +63,6 @@ public class DubuBaseTest {
     @Test
     public void afjlasjd(){
 
-
         //ImmutableList<String> a = Lists.newArrayList("aa","bb");
 
         List<String> b = Lists.newArrayList("aa","bb");
@@ -70,10 +72,93 @@ public class DubuBaseTest {
 
 
         assertThat(b).isEqualTo(asfd);
+    }
 
 
+    @Test
+    public void arrayCopyTset(){
+
+        String[] core =  new String[]{"dd", "d" ,"ee"};
+
+        List<String> aa = Arrays.asList("a", "b", "c", "e");
+
+        Lists.asList("A" , "b", core);
 
 
+        ///
+
+        List<String> k  = Arrays.asList("aaa", "bbbb" ,"ccc" , "dddd");
+        List<String> kk  = Arrays.asList("000", "111" ,"222" , "3333");
+        ImmutableList<String> kkk  = ImmutableList.of("kk", "ggg");
+
+
+        ImmutableList<String> zzz =  new ImmutableList.Builder<String>()
+                .add("..aa")
+                .add("..bb")
+                .addAll(k.iterator())
+                .addAll(kk)
+                .addAll(kkk)
+                .build();
+
+        logger.info("ASdfasfd");
+
+    }
+
+
+    @Test
+    public void afasdf(){
+
+        Iterator<String>  iterator =  Iterators.singletonIterator("A");
+
+        while (iterator.hasNext()) {
+            Object next =  iterator.next();
+            logger.info(String.valueOf(next));
+
+        }
+
+        ImmutableList<Integer> asdfaasf  = ImmutableList.of(1, 2, 124312, 534534);
+
+          for(Integer i : asdfaasf){
+
+              logger.info(String.valueOf(i));
+
+        }
+    }
+
+
+    @Test
+    public void logListToStrigList(){
+
+
+        List<String> strList = ImmutableList.of("123" , "435");
+
+        Converter<String , Integer> strToIntger  = new Converter<String, Integer>() {
+            @Override
+            protected Integer doForward(String s) {
+                return Integer.valueOf(s);
+            }
+
+            @Override
+            protected String doBackward(Integer integer) {
+                return String.valueOf(integer);
+            }
+        };
+
+        Iterable<Integer> intList =  strToIntger.convertAll(strList);
+
+        for (Integer i  : intList){
+            i.toString();
+        }
+
+    }
+
+
+    @Test
+    public void joinnerTest(){
+
+
+        List<String> aa = ImmutableList.of("aaaa" , "bbbb" , "11");
+        logger.info(Joiner.on(",,").join(aa));
     }
 
 
