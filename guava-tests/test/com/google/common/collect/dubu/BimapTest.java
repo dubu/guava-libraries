aa2dc07497126c6d7a2df8e65644ef63a85f114f
kozazz <kozazz@hanmail.net>
2015-07-08 14:18:10 +0900

    bimap test

---
+++ guava-tests/test/com/google/common/collect/dubu/BimapTest.java
@@ -0,0 +1,32 @@
+package com.google.common.collect.dubu;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import static com.google.common.collect.Lists.*;
+
+
+/**
+ * Created by rigel on 7/8/15.
+ */
+public class BimapTest {
+
+    private static final Logger logger = Logger.getLogger("com.google.common.collect.dubu.BimapTest");
+
+    @Test
+    public void testag(){
+
+        logger.info("Afda");
+
+        ImmutableSet<String> set = ImmutableSet.of("a","b","c");
+        List bb = Lists.newArrayList("a", "b", "c", 1,2,32);
+        Assert.assertEquals(set ,bb);
+
+    }
+
+}
